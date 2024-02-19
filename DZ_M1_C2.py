8809acda8d6a80a566cc881df5fb95f2854f822a
flesher-dizainer <dizainer@list.ru>
2024-02-19 21:44:23 +0300

    rewrite code

--- DZ_M1_C2.py
+++ DZ_M1_C2.py
@@ -2,39 +2,45 @@ def zadanie_1():
     a = float(input("a "))
     b = float(input("b "))
     c = float(input("c "))
-    print ('Addition = '+str(a+b+c))
-    print ('Multiplication = '+str(a*b*c))
-    
+    print ('Addition = ' + str(a + b + c))
+    print ('Multiplication = ' + str(a * b * c))
+
+
 def zadanie_2():
     a = float(input("zarplata "))
     b = float(input("platez credita "))
     c = float(input("platez communal uslugi "))
     summa = a - b - c
     if summa < 0:
-        print ('ne xvatilo deneg '+str(summa))
+        print ('ne xvatilo deneg ' + str(summa))
     else:
-        print ('ostalos deneg '+str(summa))
-        
+        print ('ostalos deneg ' + str(summa))
+
+
 def zadanie_3():
     a = float(input('diagonal a '))
     b = float(input('diagonal b '))
     s = a * b / 2
-    print ('ploshad romba = '+str(s))
- 
+    print ('ploshad romba = ' + str(s))
+
+
 def zadanie_4():
-    ex = 'To be\n'+'or not\n'+'to be'
+    ex = 'To be\n' + 'or not\n' + 'to be'
     print (ex)
 
-def zadanie_5():
-    ex = 'Life is what happens\n\r\t'+'when\r\n'+('\t'*2)+'you’re busy making other plans”\n'+('\t'*5)+'John Lennon.'
-    print (ex)  
-    
+
+def zadanie_5(n=None):
+    ex = 'Life is what happens\n\t when\n\t\t you`re busy making other plans\n\t\t\t\t\t John Lennon.'
+    print (ex)
+
+
 def main():
-    zadanie_1()
-    zadanie_2()
-    zadanie_3()
-    zadanie_4()
+    #zadanie_1()
+    #zadanie_2()
+    #zadanie_3()
+    #zadanie_4()
     zadanie_5()
-    
-if	__name__	==	'__main__':
-	main()
+
+
+if __name__ == '__main__':
+    main()
